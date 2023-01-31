6e25eb0e2bf437cdd0b98c3378349c7fb23f7d5c
ChevalierSoft <ibanarashi@outlook.fr>
2023-01-31 10:12:34 +0100

    working but with one client by start.

--- main.go
+++ main.go
@@ -4,34 +4,45 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/gin-contrib/cors"
 	gin "github.com/gin-gonic/gin"
+	"github.com/go-vgo/robotgo"
 
 	"github.com/gorilla/websocket"
 )
 
 type Vel struct {
-	X string `json:"x"`
-	Y string `json:"y"`
+	X int64 `json:"x"`
+	Y int64 `json:"y"`
+}
+
+func printIp() {
+	host, _ := os.Hostname()
+	addrs, _ := net.LookupIP(host)
+	fmt.Printf("HOST %v :\n", host)
+	for _, addr := range addrs {
+		if ipv4 := addr.To4(); ipv4 != nil {
+			fmt.Println("\t", ipv4)
+		}
+	}
 }
 
 func main() {
 	router := gin.Default()
 	router.Use(cors.Default())
 
-	// load the folder containing our static web assets
-	router.LoadHTMLGlob("public/*")
-
-	// serve the index page
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", nil)
-	})
 	router.GET("/ws", func(c *gin.Context) {
-		serveWs(c.Writer, c.Request)
+		go serveWs(c.Writer, c.Request)
 	})
+
+	router.Static("/public", "./public")
+
+	go printIp()
 	router.Run(":8080")
 }
 
@@ -54,24 +65,26 @@ func serveWs(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(string(p))
-
-		// if the first 3 characters of the message are "mos"
-		if strings.HasPrefix(string(p), "mos") {
-			// remove the first 3 characters
+		// mice position
+		if strings.HasPrefix(string(p), "pos") {
 			p = p[3:]
-			// convert string to Vel struct
-			var vle Vel
-			json.Unmarshal(p, &vle)
-			fmt.Println(vle.X, vle.Y)
+			var vel Vel
+			err = json.Unmarshal(p, &vel)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println("vel : ", vel)
+				x, y := robotgo.GetMousePos()
+				robotgo.Move(x+int(vel.X), y+int(vel.Y))
+			}
+		} else if strings.HasPrefix(string(p), "left") {
+			fmt.Printf("left\n")
+			robotgo.Click("left")
+		} else if strings.HasPrefix(string(p), "right") {
+			fmt.Printf("right\n")
+			robotgo.Click("right")
+		} else {
+			fmt.Println("not supported : ", string(p))
 		}
-
-		// convert string to Vel struct
-
-		// var vle Vel
-		// json.Unmarshal(p, &vle)
-		// fmt.Println(vle.X, vle.Y)
-
-		// robotgo.KeyTap(string(p))
 	}
 }
